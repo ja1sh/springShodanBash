566deb3563054434cb1029a2bfe50e6a36e5721c
Jai Kumar Sharma <jaisharmafps@gmail.com>
2020-08-01 15:37:55 +0530

    Bash script 1.1

---
+++ shodanSpring.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                        
+line="$1"                                                                                                          
+echo 'Scanning for' $line                                                                                          
+shodan search org:$line http.favicon.hash:116323821 --fields ip_str,port --separator " " | awk '{print $1":"$2}' | while read host do ;do ffuf -u http://$host/FUZZ -mc 200 -w /usr/share/seclists/Discovery/Web-Content/spring-boot.txt ;done | tee -a /root/Downloads/bugbounty/takeaway_Spring_FFUF.txt
